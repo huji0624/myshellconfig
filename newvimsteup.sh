ad8d50bd0966996e72aef82789f82d7b5f0caf96
Luckyhu <huji0624@gmail.com>
2018-04-17 08:52:39 +0800

    Update newvimsteup.sh

--- newvimsteup.sh
+++ newvimsteup.sh
@@ -4,7 +4,5 @@ mkdir -p ~/.vim/bundle
 curdir=$(cd `dirname $0`;pwd)
 ln $curdir/vimrc ~/.vimrc
 ln $curdir/screenrc ~/.screenrc
-ln $curdir/profile ~/.profile
-source ~/.profile
 
 echo config vimrc screenrc profile done.
